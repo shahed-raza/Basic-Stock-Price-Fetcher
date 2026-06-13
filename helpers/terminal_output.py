9b481acdef295ee39fbccd63e4a490e7ba862c88
Humza <humza0406@gmail.com>
2026-06-13 17:09:24 +0000

    Refactor (No behaviour change)
    - Rename price to last_price
    - Replace (price * quantity) with new added investement field
    of stocks_info dict

--- helpers/terminal_output.py
+++ helpers/terminal_output.py
@@ -2,12 +2,12 @@ import tabulate
 
 
 def print_table(stocks_info):
-    header = ["Stock Symbol", "Price", "Quantity", "Investment"]
+    header = ["Stock Symbol", "Last Price", "Quantity", "Investment"]
     rows = []
     for stock in stocks_info.keys():
-        price = stocks_info[stock]["price"]
+        price = stocks_info[stock]["last_price"]
         quantity = stocks_info[stock]["quantity"]
-        investment = price * quantity 
+        investment = stocks_info[stock]["investment"]
         row = [stock, to_usd(price), quantity, to_usd(investment)]
         rows.append(row)
     table = tabulate.tabulate(rows, header, "simple_grid")
